d7f7295e19e8aef44d3320b40d2d9d3b7745ee1b
youssmida <68444709+youssmida@users.noreply.github.com>
2020-11-20 20:11:27 +0100

    Add files via upload

---
+++ entrenement.js
@@ -0,0 +1,9 @@
+var est = document.getElementById(e)
+var ast = document.getElementById(a)
+var zst = document.getElementById(z)
+var pst = document.getElementById(p)
+const i = 1
+addEventListener.onclick = function () {
+    return (.y()+i)
+
+    };
